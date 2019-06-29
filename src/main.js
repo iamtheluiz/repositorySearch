43ddf7d3002ad70c6a0fbf88cde90828324c60f3
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-06-28 22:54:22 -0300

    Program can get respository information

--- src/main.js
+++ src/main.js
@@ -4,20 +4,32 @@ class App{
     constructor(){
         this.repositories = []; // Stores all repositories data
         this.repositoryForm = document.getElementById("repositoryForm");
-        this.repositoryNameInput = document.getElementById("repositoryForm");
+        this.repositoryNameInput = document.getElementById("repositoryName");
 
         // Listen when the page form is submited
         this.startListeningRepositoryForm();
     }
 
     startListeningRepositoryForm(){
-        this.repositoryForm.onsubmit = () => addRepository(this.repositoryNameInput.value);
+        this.repositoryForm.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(formData){
-        axios.get('https://api.github.com/users/iamtheluiz')
-            .then((response) => {
-                console.log(response.data);
-            })
+    async addRepository(event){
+        // Prevents that the form redirects to another page
+        event.preventDefault();
+
+        //Get repository information
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${this.repositoryNameInput.value}`)
+
+            // Clear the input field
+            this.repositoryNameInput.value = '';
+        
+            const { name, description, html_url, owner: {avatar_url} } = response.data;
+        } catch (error) {
+            console.warn(error);
+        }
     }
 }
+
+new App();
